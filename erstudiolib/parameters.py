684d2663585dd83bffcb60be51a216d11c7c2cd0
Bjgilles <602132+Bjgilles@users.noreply.github.com>
2022-06-15 16:00:37 -0700

    Delete erstudiolib directory

--- erstudiolib/parameters.py
+++
@@ -1,9 +0,0 @@
-from os import getlogin
-from os import getenv
-
-class Parameters():
-    def __init__(self):
-        self.username = getlogin()
-        self.orapass = getenv("ORAPASS")
-        self.idirpass = getenv("idirpass")
-        self.githubtoken = getenv("gittoken")
